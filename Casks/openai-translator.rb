ed4ff3bb7eca59d1a57239445b8c5f82a7764f0a
Xie Yanbo <xieyanbo@gmail.com>
2024-07-04 11:13:25 +0800

    update to v0.4.21

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,17 +1,17 @@
 cask "openai-translator" do
     arch arm: "aarch64", intel: "x64"
 
-    version "0.0.36"
-    sha256 arm: "0edfecd0984f36d5828f45c60e985b570862f2d9fefa1851f92799ec6cec630a",
-         intel: "55cff09700d3dd4f634142e1fd360b8c2b5289e79f8a1557d06a278d6ad1c6c8"
-  
-    url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
+    version "0.4.21"
+    sha256 arm: "71124edfa7dc61a57dd2eb3cc92ffa680bd1fe64af926414162169b687eabb99",
+         intel: "358d70a841fac060abf8824182e7a78194f030018a6379fdb5ca58d4d7a0b43e"
+
+    url "https://github.com/openai-translator/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
     name "OpenAI Translator"
     desc "Cross-platform desktop application for translation based on ChatGPT API."
-    homepage "https://github.com/yetone/openai-translator"
-  
+    homepage "https://github.com/openai-translator/openai-translator"
+
     app "OpenAI Translator.app"
-    
+
     postflight do
         system_command '/usr/bin/xattr',
                 args: [
